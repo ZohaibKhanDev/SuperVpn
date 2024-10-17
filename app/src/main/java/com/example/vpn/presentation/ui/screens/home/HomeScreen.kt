479269e1850009790e4aa79b1e9c511bdc5ac971
ZohaibKhanDev <mzkhan9610@gmail.com>
2024-10-17 13:18:12 +0500

    Ui Enhance

--- app/src/main/java/com/example/vpn/presentation/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/vpn/presentation/ui/screens/home/HomeScreen.kt
@@ -1,11 +1,23 @@
 package com.example.vpn.presentation.ui.screens.home
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -15,13 +27,23 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.vpn.R
 import com.example.vpn.domain.model.vpn.VpnDataResponse
 import com.example.vpn.domain.usecase.ResultState
 import com.example.vpn.presentation.viewmodel.MainViewModel
 import org.koin.compose.koinInject
 
-@SuppressLint("MutableCollectionMutableState")
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("MutableCollectionMutableState", "UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun HomeScreen() {
     var isLoading by remember {
@@ -56,28 +78,74 @@ fun HomeScreen() {
         }
     }
 
-    if (isLoading) {
-        CircularProgressIndicator()
-    }
-
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        if (isConnected) {
-            Text(text = "Connected")
-        } else {
-            Text(text = "Disconnected")
-        }
-        Button(
-            onClick = {
-                if (isConnected) {
-                    viewModel.disconnectVpn()
-                } else {
-                    isLoading = true
-                    viewModel.connectVpn()
-                }
-            },
-            modifier = Modifier.padding(top = 16.dp)
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0XFF2e2e3d)),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
         ) {
-            Text(text = if (isConnected) "Disconnect" else "Connect")
+            Image(
+                painter = painterResource(id = R.drawable.cover),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .alpha(0.7f)
+            )
+
+            Image(
+                painter = painterResource(id = R.drawable.cover),
+                contentDescription = "",
+                contentScale = ContentScale.Crop,
+                colorFilter = ColorFilter.tint(Color.LightGray.copy(alpha = 0.20f)),
+                modifier = Modifier.fillMaxSize()
+            )
+
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.spacedBy(20.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 40.dp, start = 10.dp, end = 10.dp),
+                    verticalAlignment = Alignment.Top,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(5.dp))
+                            .width(35.dp)
+                            .height(30.dp)
+                            .background(Color(0XFF3a3a4d)), contentAlignment = Alignment.Center
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Menu,
+                            contentDescription = "",
+                            tint = Color.White
+                        )
+                    }
+
+                    Text(
+                        text = "VPNite",
+                        fontSize = 23.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+                    )
+
+
+                    Image(
+                        painter = painterResource(id = R.drawable.premium),
+                        contentDescription = "",
+                        contentScale = ContentScale.Crop, modifier = Modifier.size(40.dp)
+                    )
+
+                }
+            }
         }
     }
 }
